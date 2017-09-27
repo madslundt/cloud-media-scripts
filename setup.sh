7892e1abc9a537580fd1e344a6ed0e0dabdcbcdd
Mads Engel Lundt <madslundt@live.dk>
2017-09-27 18:23:28 +0200

    INSTALL easy script

--- setup.sh
+++ setup.sh
@@ -14,71 +14,96 @@ _plexdrive_bin="plexdrive-linux-amd64"
 _plexdrive_url="https://github.com/dweidenfeld/plexdrive/releases/download/4.0.0/${_plexdrive_bin}"
 ###################################
 
-apt-get update
-apt-get install unionfs-fuse -y
-apt-get install bc -y
-apt-get install screen -y
-apt-get install unzip -y
-apt-get install fuse -y
-apt-get install golang -y
+if [ ! -f "${plexdrive_bin}" && ! -f "${rclone_bin}" ]; then
+    apt-get update
+    apt-get install unionfs-fuse -y
+    apt-get install bc -y
+    apt-get install screen -y
+    apt-get install unzip -y
+    apt-get install fuse -y
+    apt-get install golang -y
 
-if [ ! -d "${rclone_dir}" ]; then
-    mkdir -p "${rclone_dir}"
-fi
-wget "${_rclone_url}"
-unzip "${_rclone_zip}"
-chmod a+x "${_rclone_release}/rclone"
-cp -rf "${_rclone_release}/"* "${rclone_dir}/"
-rm -rf "${_rclone_zip}"
-rm -rf "${_rclone_release}"
-
-
-if [ ! -d "${plexdrive_dir}" ]; then
-    mkdir -p "${plexdrive_dir}"
-fi
-wget "${_plexdrive_url}"
-chmod a+x "${_plexdrive_bin}"
-cp -rf "${_plexdrive_bin}" "${plexdrive_dir}/"
-rm -rf "${_plexdrive_bin}"
+    if [ ! -d "${rclone_dir}" ]; then
+        mkdir -p "${rclone_dir}"
+    fi
+    wget "${_rclone_url}"
+    unzip "${_rclone_zip}"
+    chmod a+x "${_rclone_release}/rclone"
+    cp -rf "${_rclone_release}/"* "${rclone_dir}/"
+    rm -rf "${_rclone_zip}"
+    rm -rf "${_rclone_release}"
 
 
-if [ ! -d "${local_decrypt_dir}" ]; then
-    mkdir -p "${local_decrypt_dir}"
-fi
+    if [ ! -d "${plexdrive_dir}" ]; then
+        mkdir -p "${plexdrive_dir}"
+    fi
+    wget "${_plexdrive_url}"
+    chmod a+x "${_plexdrive_bin}"
+    cp -rf "${_plexdrive_bin}" "${plexdrive_dir}/"
+    rm -rf "${_plexdrive_bin}"
 
-if [ ! -d "${plexdrive_temp_dir}" ]; then
-    mkdir -p "${plexdrive_temp_dir}"
-fi
 
+    if [ ! -d "${local_decrypt_dir}" ]; then
+        mkdir -p "${local_decrypt_dir}"
+    fi
 
-echo "\n\n--------- SETUP RCLONE ----------\n"
-
-echo "1. Now run rclone with the command:"
-echo "\t${rclone_bin} --config=${rclone_config} config"
-
-if [ "$encrypt_media" -eq "0" ]; then
-    echo "2. You need to setup 1 endpoint to Google Drive remote. This is only used to upload media to your Google Drive account"
+    if [ ! -d "${plexdrive_temp_dir}" ]; then
+        mkdir -p "${plexdrive_temp_dir}"
+    fi
 else
-    echo "2. You need to setup 3 endpoints:"
-    echo "\t- Google Drive remote"
-    echo "\t- Crypt for your Google Drive remote named '${rclone_cloud_endpoint%?}'"
-    echo "\t- Crypt for your local directory ('${cloud_encrypt_dir}') named '${rclone_local_endpoint%?}'"
+    echo "Rclone and Plexdrive are already installed.\n"
 fi
 
-
-echo "\n\n-------- SETUP PLEXDRIVE --------\n"
-
-mongo="--mongo-database=${mongo_database} --mongo-host=${mongo_host}"
-if [ ! -z "${mongo_user}" -a "${mongo_user}" != " " ]; then
-    mongo="${mongo} --mongo-user=${mongo_user} --mongo-password=${mongo_password}"
+if [ -f $rclone_config ]; then
+    echo "Rclone config already exists"
+    rcloneInstallText="Do you want to change Rclone config now"
+else
+    echo "No Rclone config exists"
+    rcloneInstallText="Do you want to set up Rclone now"
 fi
 
-echo "1. Now run plexdrive with the command:"
-echo "\t${plexdrive_bin} --config=${plexdrive_dir} ${mongo} ${cloud_encrypt_dir}"
-echo "2. Enter authorization"
-echo "3. Cancel plexdrive by pressing CTRL+C"
+read -e -p "${rcloneInstallText} [Y/n]? " -i "y" rcloneSetup
+echo "=======RCLONE========\n"
 
-echo "\n\n"
+if [ "${rcloneSetup,,}" == "y"  ]; then
+    cp "${rclone_dir}/rclone.template.conf" "${rclone_config}"
+    echo "\t+ Google Drive credentials are needed within Rclone. This must be added to the Rclone remote 'gd'"
+    if [ "$encrypt_media" != "0" ]; then
+        sed -i "s|<GOOGLE_DRIVE_MEDIA_DIRECTORY>|$google_drive_media_directory|g" "${rclone_config}"
+        sed -i "s|[gd-crypt]|${rclone_cloud_endpoint%?}|g" "${rclone_config}"
+        sed -i "s|<ENCRYPTED_FOLDER>|$cloud_encrypt_dir|g" "${rclone_config}"
+        echo "\t+ Password and a salt are needed within Rclone when using encryption. This must be added to the Rclone remote '${rclone_cloud_endpoint%?}' and '${rclone_local_endpoint%?}'"
+    fi
+    echo "\nWhen this is done exit rclone config by pressing 'Q'"
+    ${rclone_bin} --config=${rclone_config} config
+    echo "Rclone has successfully been updated"
+fi
 
-echo "Start mount by running the mount.remote [${media_dir}/scripts/mount.remote]"
-echo "This can take a while because Plexdrive begins to cache your files"
+if [ ! -f "${plexdrive_dir}/token.json" ]; then
+    read -e -p "Do you want to set up Plexdrive now [Y/n]? " -i "y" plexdriveSetup
+    echo "======PLEXDRIVE======\n"
+
+    if [ "${plexdriveSetup,,}" == "y"  ]; then
+        mongo="--mongo-database=${mongo_database} --mongo-host=${mongo_host}"
+        if [ ! -z "${mongo_user}" -a "${mongo_user}" != " " ]; then
+            mongo="${mongo} --mongo-user=${mongo_user} --mongo-password=${mongo_password}"
+        fi
+        echo "After credentials have been added exit Plexdrive by pressing CTRL+C"
+        ${plexdrive_bin} --config=${plexdrive_dir} ${mongo} ${cloud_encrypt_dir}
+        echo "Plexdrive has successfully been updated"
+    fi
+else
+    echo "Plexdrive config already exists."
+fi
+
+echo "\n"
+read -e -p "Do you want to start mounting now [Y/n]? " -i "y" mountStart
+
+if [ "${mountStart,,}" == "y"  ]; then
+    echo "\nThis may take a while because Plexdrive needs to cache your files"
+    ${media_dir}/scripts/mount.remote
+    echo "Mount is up and running"
+else
+    echo "\nStart mount later by running the mount.remote [${media_dir}/scripts/mount.remote]"
+    echo "Or running setup again"
+fi
